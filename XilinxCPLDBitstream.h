170d69282d9abfd427e6e9f9d3d7b1ce94d44fec
Andrew Zonenberg <azonenberg@drawersteak.com>
2017-06-13 00:32:04 -0700

    XilinxCPLDBitstream: moved from legacy to main, removed trailing spaces

--- XilinxCPLDBitstream.h
+++ XilinxCPLDBitstream.h
@@ -2,7 +2,7 @@
 *                                                                                                                      *
 * ANTIKERNEL v0.1                                                                                                      *
 *                                                                                                                      *
-* Copyright (c) 2012-2016 Andrew D. Zonenberg                                                                          *
+* Copyright (c) 2012-2017 Andrew D. Zonenberg                                                                          *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -49,7 +49,7 @@ class XilinxCPLDBitstream : public CPLDBitstream
 public:
 	XilinxCPLDBitstream();
 	virtual ~XilinxCPLDBitstream();
-	
+
 	virtual std::string GetDescription();
 };
 
