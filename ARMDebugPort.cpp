784857e1dae97a832bd2520b6058d367b749aa21
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2018-05-04 12:27:15 -0700

    ARMDebugPort: removed trailing spaces

--- ARMDebugPort.cpp
+++ ARMDebugPort.cpp
@@ -2,7 +2,7 @@
 *                                                                                                                      *
 * ANTIKERNEL v0.1                                                                                                      *
 *                                                                                                                      *
-* Copyright (c) 2012-2016 Andrew D. Zonenberg                                                                          *
+* Copyright (c) 2012-2018 Andrew D. Zonenberg                                                                          *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -53,13 +53,13 @@ ARMDebugPort::ARMDebugPort(
 	, m_partnum(partnum)
 {
 	m_irlength = 4;
-	
+
 	//No Mem-AP for now
 	m_defaultMemAP = NULL;
-	
+
 	//Turn on the debug stuff
 	EnableDebugging();
-	
+
 	//Figure out how many APs we have
 	uint8_t nap = 0;
 	for(; nap<255; nap++)
@@ -68,7 +68,7 @@ ARMDebugPort::ARMDebugPort(
 		idr.word = APRegisterRead(nap, REG_IDR);
 		if(idr.word == 0)
 			break;
-	
+
 		//Sanity check that it's a valid ARM DAP
 		if( (idr.bits.continuation != 0x4) || (idr.bits.identity != 0x3b) )
 		{
@@ -77,17 +77,17 @@ ARMDebugPort::ARMDebugPort(
 				"",
 				JtagException::EXCEPTION_TYPE_GIGO);
 		}
-		
+
 		//If it's a JTAG-AP, skip it for now
 		if(!idr.bits.is_mem_ap)
 			continue;
-			
+
 		//Create a new MEM-AP
 		else
 		{
 			ARMDebugMemAccessPort* ap = new ARMDebugMemAccessPort(this, nap, idr);
-			m_aps[nap] = ap;			
-			
+			m_aps[nap] = ap;
+
 			//If it's an AHB-AP, and we don't have a default Mem-AP, this one is probably RAM.
 			//Use it as our default AP.
 			if( (ap->GetBusType() == ARMDebugAccessPort::DAP_AHB) && (m_defaultMemAP == NULL) )
@@ -114,15 +114,15 @@ JtagDevice* ARMDebugPort::CreateDevice(
 	{
 	case IDCODE_ARM_DAP_JTAG:
 		return new ARMDebugPort(partnum, rev, idcode, iface, pos);
-		
+
 	default:
 		fprintf(stderr, "ARMDebugPort::CreateDevice(partnum=%x, rev=%x, idcode=%08x)\n", partnum, rev, idcode);
-	
+
 		throw JtagExceptionWrapper(
 			"Unknown ARM debug device (ID code not in database)",
 			"",
 			JtagException::EXCEPTION_TYPE_GIGO);
-	}	
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -137,12 +137,12 @@ void ARMDebugPort::EnableDebugging()
 		printf("    Error bit is set, clearing\n");
 		ClearStatusRegisterErrors();
 	}
-	
+
 	//Power up the system
 	stat.word = 0;
 	stat.bits.sys_pwrup_req = 1;
 	DPRegisterWrite(REG_CTRL_STAT, stat.word);
-	
+
 	//Verify it powered up OK
 	stat = GetStatusRegister();
 	if(!stat.bits.sys_pwrup_ack)
@@ -152,12 +152,12 @@ void ARMDebugPort::EnableDebugging()
 			"",
 			JtagException::EXCEPTION_TYPE_BOARD_FAULT);
 	}
-	
+
 	//Power up the debug logic
 	stat.word = 0;
 	stat.bits.debug_pwrup_req = 1;
 	DPRegisterWrite(REG_CTRL_STAT, stat.word);
-	
+
 	//Verify it powered up OK
 	stat = GetStatusRegister();
 	if(!stat.bits.debug_pwrup_ack)
@@ -183,7 +183,7 @@ uint32_t ARMDebugPort::ReadMemory(uint32_t address)
 			"",
 			JtagException::EXCEPTION_TYPE_BOARD_FAULT);
 	}
-	
+
 	//Use the default Mem-AP to read it
 	return m_defaultMemAP->ReadWord(address);
 }
@@ -199,7 +199,7 @@ void ARMDebugPort::WriteMemory(uint32_t address, uint32_t value)
 			"",
 			JtagException::EXCEPTION_TYPE_BOARD_FAULT);
 	}
-	
+
 	//Use the default Mem-AP to write it
 	m_defaultMemAP->WriteWord(address, value);
 }
@@ -210,23 +210,23 @@ void ARMDebugPort::WriteMemory(uint32_t address, uint32_t value)
 std::string ARMDebugPort::GetDescription()
 {
 	string devname = "(unknown)";
-	
+
 	switch(m_partnum)
 	{
 	case IDCODE_ARM_DAP_JTAG:
 		devname = "JTAG DAP";
 		break;
-		
+
 	default:
 		throw JtagExceptionWrapper(
 			"Unknown ARM device (ID code not in database)",
 			"",
 			JtagException::EXCEPTION_TYPE_GIGO);
 	}
-	
+
 	char srev[16];
 	snprintf(srev, 15, "%u", m_rev);
-	
+
 	return string("ARM ") + devname + " rev " + srev;
 }
 
@@ -252,7 +252,7 @@ void ARMDebugPort::PrintStatusRegister(ARMDebugPortStatusRegister reg)
 	printf("    Transfer mode:     %u\n", reg.bits.transfer_mode);
 	printf("    Sticky overrun:    %u\n", reg.bits.sticky_overrun);
 	printf("    Sticky overrun en: %u\n", reg.bits.sticky_overrun_en);
-	
+
 	for(auto x : m_aps)
 		x.second->PrintStatusRegister();
 }
@@ -277,10 +277,10 @@ void ARMDebugPort::ClearStatusRegisterErrors()
 
 /**
 	@brief Reads from an AP register
-	
+
 	@param ap			The number of the AP to access
 	@param addr			The ID of the AP register to read
-	
+
 	@return The value read
  */
 uint32_t ARMDebugPort::APRegisterRead(uint8_t ap, ApReg addr)
@@ -288,17 +288,17 @@ uint32_t ARMDebugPort::APRegisterRead(uint8_t ap, ApReg addr)
 	//Set the high bits of the address as the current bank
 	uint32_t select = (ap << 24) | (addr & 0xf0);
 	DPRegisterWrite(REG_AP_SELECT,  select );
-	
+
 	//Do the read
 	SetIR(INST_APACC);
-	
+
 	uint32_t data_out;
-	
+
 	//Poll until we get a good read back
 	int i = 0;
 	int nmax = 100;
 	for(; i<nmax; i++)
-	{	
+	{
 		//Send the 3-bit A / RnW field to request the read
 		uint8_t addr_flags = ((addr & 0x0c) >> 1) | OP_READ;
 		//printf("        addr_flags = %x\n", addr_flags);
@@ -315,29 +315,29 @@ uint32_t ARMDebugPort::APRegisterRead(uint8_t ap, ApReg addr)
 				JtagException::EXCEPTION_TYPE_UNIMPLEMENTED);
 			*/
 		}
-		
+
 		//Send some dummy data
 		unsigned char unused[4] = {0};
 		unsigned char unused2[4] = {0};
 		m_iface->ShiftData(1, unused, unused2, 32);
 		m_iface->LeaveExit1DR();
-			
+
 		//Send the same A / RnW field again to get the response data
 		m_iface->EnterShiftDR();
 		m_iface->ShiftData(0, &addr_flags, &ack_out, 3);
-		
+
 		//Send some dummy data and get the read value back
 		m_iface->ShiftData(1, unused, (unsigned char*)&data_out, 32);
 		m_iface->LeaveExit1DR();
-		
+
 		//If we got a success result, done
 		if(ack_out == OK_OR_FAULT)
 			break;
-		
+
 		//No go? Try again after a millisecond
 		usleep(1 * 1000);
 	}
-	
+
 	//Give up if we still got nothing
 	if(i == nmax)
 	{
@@ -348,7 +348,7 @@ uint32_t ARMDebugPort::APRegisterRead(uint8_t ap, ApReg addr)
 			"",
 			JtagException::EXCEPTION_TYPE_BOARD_FAULT);
 	}
-	
+
 	//Verify the read was successful
 	ARMDebugPortStatusRegister stat = GetStatusRegister();
 	if(stat.bits.sticky_err)
@@ -360,7 +360,7 @@ uint32_t ARMDebugPort::APRegisterRead(uint8_t ap, ApReg addr)
 			"",
 			JtagException::EXCEPTION_TYPE_BOARD_FAULT);
 	}
-	
+
 	return data_out;
 }
 
@@ -371,7 +371,7 @@ void ARMDebugPort::DebugAbort()
 {
 	SetIR(INST_ABORT);
 	m_iface->EnterShiftDR();
-	
+
 	//Write to the abort register
 	//TODO: make a bit field or something
 	uint32_t abort_value = 0x1;
@@ -382,7 +382,7 @@ void ARMDebugPort::DebugAbort()
 
 /**
 	@brief Writes to an AP register
-	
+
 	@param ap			The number of the AP to access
 	@param addr			The ID of the AP register to read
 	@param wdata		The value to write
@@ -392,22 +392,22 @@ void ARMDebugPort::APRegisterWrite(uint8_t ap, ApReg addr, uint32_t wdata)
 	//Set the high bits of the address as the current bank
 	uint32_t select = (ap << 24) | (addr & 0xf0);
 	DPRegisterWrite(REG_AP_SELECT,  select );
-	
+
 	//Do the read
 	SetIR(INST_APACC);
-	
+
 	//Send the 3-bit A / RnW field to request the write
 	uint8_t addr_flags = ((addr & 0x0c) >> 1) | OP_WRITE;
 	//printf("        addr_flags = %x\n", addr_flags);
 	uint8_t ack_out;
 	m_iface->EnterShiftDR();
 	m_iface->ShiftData(0, &addr_flags, &ack_out, 3);
-		
+
 	///Send the data being written
 	unsigned char unused[4] = {0};
 	m_iface->ShiftData(1, (unsigned char*)&wdata, unused, 32);
 	m_iface->LeaveExit1DR();
-	
+
 	//If the original ACK-out was a "wait", we have to do something
 	if(ack_out == WAIT)
 	{
@@ -416,7 +416,7 @@ void ARMDebugPort::APRegisterWrite(uint8_t ap, ApReg addr, uint32_t wdata)
 			"",
 			JtagException::EXCEPTION_TYPE_UNIMPLEMENTED);
 	}
-		
+
 	//Send a dummy read to get the response code
 	addr_flags = ((addr & 0x0c) >> 1) | OP_READ;
 	m_iface->EnterShiftDR();
@@ -428,12 +428,12 @@ void ARMDebugPort::APRegisterWrite(uint8_t ap, ApReg addr, uint32_t wdata)
 			"",
 			JtagException::EXCEPTION_TYPE_UNIMPLEMENTED);
 	}
-	
+
 	//Send some dummy data
 	uint32_t data_out;
 	m_iface->ShiftData(1, unused, (unsigned char*)&data_out, 32);
 	m_iface->LeaveExit1DR();
-	
+
 	//Verify the read was successful
 	ARMDebugPortStatusRegister stat = GetStatusRegister();
 	if(stat.bits.sticky_err)
@@ -449,9 +449,9 @@ void ARMDebugPort::APRegisterWrite(uint8_t ap, ApReg addr, uint32_t wdata)
 
 /**
 	@brief Reads from a DP register
-	
+
 	@param addr			The ID of the DP register to read
-	
+
 	@return The value read
  */
 uint32_t ARMDebugPort::DPRegisterRead(DpReg addr)
@@ -464,13 +464,13 @@ uint32_t ARMDebugPort::DPRegisterRead(DpReg addr)
 	m_iface->EnterShiftDR();
 	m_iface->ShiftData(0, &addr_flags, &ack_out, 3);
 	//ignore original ack out
-	
+
 	//Send some dummy data
 	unsigned char unused[4] = {0};
 	unsigned char unused2[4] = {0};
 	m_iface->ShiftData(1, unused, unused2, 32);
 	m_iface->LeaveExit1DR();
-	
+
 	//Send the same A / RnW field again to get the response data
 	m_iface->EnterShiftDR();
 	m_iface->ShiftData(0, &addr_flags, &ack_out, 3);
@@ -481,12 +481,12 @@ uint32_t ARMDebugPort::DPRegisterRead(DpReg addr)
 			"",
 			JtagException::EXCEPTION_TYPE_UNIMPLEMENTED);
 	}
-	
+
 	//Send some dummy data
 	uint32_t data_out;
 	m_iface->ShiftData(1, unused, (unsigned char*)&data_out, 32);
 	m_iface->LeaveExit1DR();
-	
+
 	return data_out;
 }
 
@@ -500,12 +500,12 @@ void ARMDebugPort::DPRegisterWrite(DpReg addr, uint32_t wdata)
 	m_iface->EnterShiftDR();
 	m_iface->ShiftData(0, &addr_flags, &ack_out, 3);
 	//ignore original ack out
-	
+
 	//Send the data being written
 	unsigned char unused[4] = {0};
 	m_iface->ShiftData(1, (unsigned char*)&wdata, unused, 32);
 	m_iface->LeaveExit1DR();
-	
+
 	//Send a read request to get the response code
 	addr_flags = (addr << 1) | OP_READ;
 	m_iface->EnterShiftDR();
@@ -517,7 +517,7 @@ void ARMDebugPort::DPRegisterWrite(DpReg addr, uint32_t wdata)
 			"",
 			JtagException::EXCEPTION_TYPE_UNIMPLEMENTED);
 	}
-	
+
 	//Send some dummy data
 	unsigned char unused2[4] = {0};
 	m_iface->ShiftData(1, unused2, unused, 32);
--- ARMDebugPort.h
+++ ARMDebugPort.h
@@ -2,7 +2,7 @@
 *                                                                                                                      *
 * ANTIKERNEL v0.1                                                                                                      *
 *                                                                                                                      *
-* Copyright (c) 2012-2016 Andrew D. Zonenberg                                                                          *
+* Copyright (c) 2012-2018 Andrew D. Zonenberg                                                                          *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -51,54 +51,54 @@ union ARMDebugPortStatusRegister
 	{
 		///Set to 1 to enable overrun detection
 		unsigned int sticky_overrun_en:1;
-		
+
 		///Sticky buffer overrun (if enabled)
 		unsigned int sticky_overrun:1;
-		
+
 		///Transfer mode
 		unsigned int transfer_mode:2;
-		
+
 		///Sticky compare bit
 		unsigned int sticky_compare:1;
-		
+
 		///Sticky error bit
 		unsigned int sticky_err:1;
-		
+
 		///Read status flag
 		unsigned int read_ok:1;
-		
+
 		///Write data error flag
 		unsigned int wr_data_err:1;
-		
+
 		///Byte mask
 		unsigned int mask_lane:4;
-		
+
 		///Transaction counter
 		unsigned int trans_count:12;
-		
+
 		///Reserved, should be zero
 		unsigned int reserved_zero:2;
-		
+
 		///Debug reset request
 		unsigned int debug_reset_req:1;
-		
+
 		///Debug reset acknowledgement
 		unsigned int debug_reset_ack:1;
-		
+
 		///Powerup request
 		unsigned int debug_pwrup_req:1;
-		
+
 		///Powerup acknowledgement
 		unsigned int debug_pwrup_ack:1;
-		
+
 		///Powerup request
 		unsigned int sys_pwrup_req:1;
-		
+
 		///Powerup acknowledgement
 		unsigned int sys_pwrup_ack:1;
-		
+
 	} __attribute__ ((packed)) bits;
-	
+
 	///The raw status register value
 	uint32_t word;
 } __attribute__ ((packed));
@@ -107,7 +107,7 @@ class ARMDebugMemAccessPort;
 
 /**
 	@brief An ARM debug port (contains one or more APs and a DP)
-	
+
 	\ingroup libjtaghal
  */
 class ARMDebugPort		: public ARMDevice
@@ -125,14 +125,14 @@ public:
 		JtagInterface* iface,
 		size_t pos);
 	virtual ~ARMDebugPort();
-	
+
 	static JtagDevice* CreateDevice(
 		unsigned int partnum,
 		unsigned int rev,
 		unsigned int idcode,
 		JtagInterface* iface,
 		size_t pos);
-		
+
 	enum instructions
 	{
 		INST_IDCODE = 0x0e,
@@ -140,38 +140,38 @@ public:
 		INST_DPACC = 0x0a,
 		INST_APACC = 0x0b,
 	};
-		
+
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// General device info
 
 	virtual std::string GetDescription();
-	
+
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// Memory access
-	
+
 	///Read a single 32-bit word of memory (TODO support smaller sizes)
 	virtual uint32_t ReadMemory(uint32_t address);
-	
+
 	///Writes a single 32-bit word of memory (TODO support smaller sizes)
 	virtual void WriteMemory(uint32_t address, uint32_t value);
-	
+
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// Helpers for debug port manipulation
-	
+
 	//Results from DAP commands
 	enum DapResult
 	{
 		OK_OR_FAULT = 2,
 		WAIT = 1
 	};
-	
+
 	//Read-write flags
 	enum RWFlag
 	{
 		OP_WRITE = 0,
 		OP_READ = 1
 	};
-	
+
 	//Well-defined DP registers
 	enum DpReg
 	{
@@ -179,7 +179,7 @@ public:
 		REG_AP_SELECT = 2,
 		REG_RDBUFF = 3
 	};
-	
+
 	//Well-defined AP registers
 	enum ApReg
 	{
@@ -187,48 +187,48 @@ public:
 		REG_MEM_TAR  = 0x04,
 		REG_MEM_DRW  = 0x0C,
 		REG_MEM_BASE = 0xF8,
-		
+
 		REG_IDR		= 0xFC
 	};
-	
+
 protected:
 	ARMDebugPortStatusRegister GetStatusRegister();
 	void ClearStatusRegisterErrors();
 	void PrintStatusRegister(ARMDebugPortStatusRegister reg);
-	
+
 	uint32_t DPRegisterRead(DpReg addr);
 	void DPRegisterWrite(DpReg addr, uint32_t wdata);
-	
+
 	//need to be a friend so that the Mem-AP can poke registers
 	//TODO: try to find a cleaner way to expose this?
 	friend class ARMDebugMemAccessPort;
 	uint32_t APRegisterRead(uint8_t ap, ApReg addr);
 	void APRegisterWrite(uint8_t ap, ApReg addr, uint32_t wdata);
-	
+
 	void EnableDebugging();
-	
+
 	void DebugAbort();
-	
+
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// Helpers for chain manipulation
-protected:	
+protected:
 	void SetIR(unsigned char irval)
 	{ JtagDevice::SetIR(&irval, m_irlength); }
-	
+
 	void SetIRDeferred(unsigned char irval)
 	{ JtagDevice::SetIRDeferred(&irval, m_irlength); }
-	
+
 protected:
-	
+
 	///Stepping number
 	unsigned int m_rev;
-	
+
 	///Part number (normally IDCODE_ARM_DAP_JTAG)
 	unsigned int m_partnum;
-	
+
 	///Access ports
 	std::map<uint8_t, ARMDebugAccessPort*> m_aps;
-	
+
 	///The default Mem-AP used for memory access
 	ARMDebugMemAccessPort* m_defaultMemAP;
 };
